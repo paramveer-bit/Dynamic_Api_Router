42366893b3c564dc5fe030f463e19bd96cb2d691
Paramveer-bit <67367312+paramveer-bit@users.noreply.github.com>
2025-02-06 21:51:08 +0530

    Added Node Mailer, Helpers Functions

---
+++ src/helpers/ApiResponse.ts
@@ -0,0 +1,15 @@
+class ApiResponse {
+    statusCode: string;
+    data: string;
+    message: string;
+    success: boolean;
+
+    constructor(statusCode: string, data: string, message = "Success") {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = parseInt(statusCode) < 400;
+    }
+}
+
+export { ApiResponse }
